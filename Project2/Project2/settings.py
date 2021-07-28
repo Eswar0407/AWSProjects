98a223ffffaf694a636f6d0fd8f498e22ab7613f
Eswar <eswarbablu@gmail.com>
2021-07-28 08:29:06 +0530

    product

--- Project2/Project2/settings.py
+++ Project2/Project2/settings.py
@@ -121,27 +121,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-#STATIC_URL = '/static/'
+STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
-AWS_ACCESS_KEY_ID = 'AKIASJXIYEYMF3JLOFDV'
-
-AWS_SECRET_ACCESS_KEY = '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'
-
-AWS_STORAGE_BUCKET_NAME = 'Eswar'
-
-AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
-AWS_S3_OBJECT_PARAMETERS = {
-    'CacheControl': 'max-age=86400',
-}
-AWS_LOCATION = 'static'
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'app2/static'),
-]
-STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
